b09e58bb2445b5a038f63560d2f4861c387dd442
shroida <shroida012@gmail.com>
2024-12-10 16:01:24 +0200

    bottom theme options

--- lib/zRefactor/features/home_page/view/widgets/app_bar_home_view.dart
+++ lib/zRefactor/features/home_page/view/widgets/app_bar_home_view.dart
@@ -1,7 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:chatoid/constants.dart';
-import 'package:chatoid/cubits/themeCubit/theme_cubit.dart';
 import 'package:chatoid/presntation/widgets/Search_widget.dart';
+import 'package:chatoid/zRefactor/features/home_page/view/widgets/show_theme_options.dart';
 import 'package:chatoid/zRefactor/features/login/view_model/login_cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,7 +19,6 @@ class _AppBarHomeViewState extends State<AppBarHomeView> {
   @override
   Widget build(BuildContext context) {
     final loginCubit = context.watch<LoginCubit>();
-    final themeCubit = context.watch<ThemeCubit>();
     return AppBar(
       leading: IconButton(
         icon: const Icon(Icons.menu),
@@ -63,7 +60,7 @@ class _AppBarHomeViewState extends State<AppBarHomeView> {
           child: IconButton(
             icon: const Icon(Icons.more_vert, size: 30),
             onPressed: () {
-              _showThemeOptions(context, themeCubit);
+              _showThemeOptions(context);
             },
           ),
         ),
@@ -72,114 +69,11 @@ class _AppBarHomeViewState extends State<AppBarHomeView> {
   }
 }
 
-Widget _buildthemeOptions(BuildContext context, ThemeCubit themeProvider) {
-  List<Color> colors = [
-    const Color.fromARGB(0, 96, 76, 212),
-    ChatAppColors.primaryColor,
-    ChatAppColors.primaryColor2,
-    ChatAppColors.primaryColor3,
-    ChatAppColors.primaryColor4,
-    ChatAppColors.primaryColor5,
-    ChatAppColors.primaryColor6,
-    ChatAppColors.primaryColor7,
-    ChatAppColors.primaryColor8,
-  ];
-
-  return Center(
-    child: CarouselSlider(
-      items: colors.map((color) {
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: const SizedBox(height: 20),
-        );
-      }).toList(),
-      options: CarouselOptions(
-        height: 20,
-        onPageChanged: (index, reason) {
-          themeProvider.toggleBackground(index);
-          themeProvider.saveColorOfApp(colors[index]);
-        },
-        enableInfiniteScroll: false,
-        initialPage: 0,
-      ),
-    ),
-  );
-}
-
-void _showThemeOptions(BuildContext context, ThemeCubit themeCubit) {
+void _showThemeOptions(BuildContext context) {
   showModalBottomSheet(
     context: context,
     builder: (context) {
-      return Wrap(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(left: 30, bottom: 30),
-                      child: const Text(
-                        'Interface Theme',
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text("Light Mode"),
-                      leading: Radio<ThemeMode>(
-                        value: ThemeMode.light,
-                        groupValue: themeCubit.themeMode,
-                        onChanged: (ThemeMode? value) {
-                          if (value != null) {
-                            themeCubit.toggleLightMode();
-                          }
-                        },
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text("Dark Mode"),
-                      leading: Radio<ThemeMode>(
-                        value: ThemeMode.dark,
-                        groupValue: themeCubit.themeMode,
-                        onChanged: (ThemeMode? value) {
-                          if (value != null) {
-                            themeCubit.toggleDarkMode();
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 60),
-                Container(
-                  margin: const EdgeInsets.only(left: 30, bottom: 20),
-                  child: const Text(
-                    'Theme Color',
-                    style: TextStyle(fontWeight: FontWeight.w900),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 30, bottom: 30),
-                  child: const Text(
-                    'Customize your application color',
-                    style: TextStyle(fontWeight: FontWeight.w400),
-                  ),
-                ),
-                _buildthemeOptions(context, themeCubit)
-              ],
-            ),
-          ),
-        ],
-      );
+      return const ShowThemeOptions();
     },
   );
 }
